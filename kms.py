cdb488a0fe9cf38b2cb23b0070f07241aaf4d23a
ZeroY <admin@zeroy.net>
2020-02-22 11:49:35 +0800

    123

--- kms.py
+++ kms.py
@@ -84,13 +84,7 @@ def getKmsKey(edition):
         'Windows 7 Professional E': 'W82YF-2Q76Y-63HXB-FGJG9-GF7QX',
         'Windows 7 Enterprise': '33PXH-7Y6KF-2VJC9-XBBR8-HVTHH',
         'Windows 7 Enterprise N': 'YDRBP-3D83W-TY26F-D46B2-XCKRJ',
-        'Windows 7 Enterprise E': 'C29WB-22CC8-VJ326-GHFJW-H9DH4',
-        'Windows Server 2016 Datacenter': 'CB7KF-BWN84-R7R2Y-793K2-8XDDG',
-        'Windows Server 2016 Standard': 'WC2BQ-8NRM3-FDDYY-2BFGV-KHKQY',
-        'Windows Server 2016 Essentials': 'JCKRF-N37P4-C2D82-9YXRT-4M63B',
-        'Windows Server 2019 Datacenter': 'WMDGN-G9PQG-XVVXX-R3X43-63DFG',
-        'Windows Server 2019 Standard': 'N69G4-B89J2-4G8F4-WWYCC-J464C',
-        'Windows Server 2019 Essentials': 'WVDHN-86M7X-466P6-VHXV7-YY726'
+        'Windows 7 Enterprise E': 'C29WB-22CC8-VJ326-GHFJW-H9DH4'
     }
     return keyDict[edition]
 
